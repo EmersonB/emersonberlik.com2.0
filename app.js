11525263d3eaab2144869299817f55bae75c89ba
EmersonB <eberlik@gmail.com>
2017-09-13 22:58:56 -0400

    cool

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ app.get('/', function(req, res) {
 
 });
 
-app.get('/.well-known/acme-challenge/mgicD-bfw1BkGhVPjlGjqo7hhRnprOLkIrDqxAheHDo', function(req, res) {
+app.get('/.well-known/acme-challenge/ScAgJW40zG95wvJHsxfDx-5r08i9V0t6cAFlvVikUWo', function(req, res) {
 
-  res.send('mgicD-bfw1BkGhVPjlGjqo7hhRnprOLkIrDqxAheHDo.G6lO1BSz-SrVAiDgA3Z6-o9sdUlLxl_N1YBRTCIpQIQ');
+  res.send('ScAgJW40zG95wvJHsxfDx-5r08i9V0t6cAFlvVikUWo.G6lO1BSz-SrVAiDgA3Z6-o9sdUlLxl_N1YBRTCIpQIQ');
 
 });
 
